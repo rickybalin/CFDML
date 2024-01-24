6b5945f7a0e47fb7696e8127c4303575d75c1f59
Riccardo Balin <balin@x1921c0s0b0n0.hostmgmt.cm.americas.sgi.com>
2024-01-24 18:46:23 +0000

    Updated OpenVINO IR of anisoSGS HIT model to FP32

--- models/anisoSGS/HIT/convert_pt2ov.py
+++ models/anisoSGS/HIT/convert_pt2ov.py
@@ -7,4 +7,4 @@ model = torch.jit.load("NNmodel_HIT_jit.pt")
 input_data = torch.rand(2048,6)
 
 ov_model = ov.convert_model(model, example_input=input_data)
-ov.save_model(ov_model, "NNmodel_HIT.xml")
+ov.save_model(ov_model, "NNmodel_HIT.xml", compress_to_fp16=False)
